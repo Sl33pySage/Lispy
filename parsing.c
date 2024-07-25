96c677ac446debe1648e6d0dcbb9e1c489e26326
Sl33pySage <jasonwolf82@hotmail.com>
2024-07-25 02:16:34 -0500

    *sips coffee*

--- parsing.c
+++ parsing.c
@@ -144,18 +144,20 @@ int main(int argc, char **argv) {
 
   /* Create Some Parsers */
   mpc_parser_t *Number = mpc_new("number");
-  mpc_parser_t *Operator = mpc_new("operator");
+  mpc_parser_t *Symbol = mpc_new("symbol");
+  mpc_parser_t *Sexpr = mpc_new("sexpr");
   mpc_parser_t *Expr = mpc_new("expr");
   mpc_parser_t *Lispy = mpc_new("lispy");
 
   /* Define them with the following Language */
 
   mpca_lang(MPCA_LANG_DEFAULT, "number : /-?[0-9]+/; \
-      operator : '+' | '-' | '*' | '/' ; \
-      expr : <number> | '(' <operator> <expr>+ ')' ; \
-      lispy : /^/ <operator> <expr>+ /$/ ; \
+      symbol : '+' | '-' | '*' | '/' ; \
+      sexpr : '(' <expr>* ')'; \
+      expr : <number> | <symbol> | <sexpr> ; \
+      lispy : /^/ <expr>* /$/ ; \
     ",
-            Number, Operator, Expr, Lispy);
+            Number, Symbol, Sexpr, Expr, Lispy);
 
   /* Print Version and Exit Information */
   puts("Lispy Version 0.0.0.0.3");
@@ -186,6 +188,6 @@ int main(int argc, char **argv) {
     free(input);
   }
   /* Undefine and Delete our Parsers */
-  mpc_cleanup(4, Number, Operator, Expr, Lispy);
+  mpc_cleanup(4, Number, Symbol, Sexpr, Expr, Lispy);
   return 0;
 }
