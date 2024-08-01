630e0f5de39087570d19190bf23fb46ee16a6ae6
Sl33pySage <jasonwolf82@hotmail.com>
2024-08-01 00:33:27 -0500

    -.- bug fixes at 12am

--- parsing.c
+++ parsing.c
@@ -438,20 +438,26 @@ lval *builtin_mul(lenv *e, lval *a) { return builtin_op(e, a, "*"); }
 lval *builtin_div(lenv *e, lval *a) { return builtin_op(e, a, "/"); }
 
 lval *builtin_def(lenv *e, lval *a) {
+
   LASSERT_TYPE("def", a, 0, LVAL_QEXPR);
+
   /* First argument is symbol list */
   lval *syms = a->cell[0];
+
   /* Ensure all elements of first list are symbols */
   for (int i = 0; i < syms->count; i++) {
     LASSERT(a, (syms->cell[i]->type == LVAL_SYM),
-            "Function 'def' cannot define non-symbol. Got %s, Expected %s.",
+            "Function 'def' cannot define non-symbol. "
+            "Got %s, Expected %s.",
             ltype_name(syms->cell[i]->type), ltype_name(LVAL_SYM));
   }
-  /* Check corrent number of symbols and values */
+
+  /* Check correct number of symbols and values */
   LASSERT(a, (syms->count == a->count - 1),
-          "Function 'def' passed too many arguents for symbols. Got %i, "
-          "Expected %i.",
+          "Function 'def' passed too many arguments for symbols. "
+          "Got %i, Expected %i.",
           syms->count, a->count - 1);
+
   /* Assign copies of values to symbols */
   for (int i = 0; i < syms->count; i++) {
     lenv_put(e, syms->cell[i], a->cell[i + 1]);
@@ -470,6 +476,10 @@ void lenv_add_builtin(lenv *e, char *name, lbuiltin func) {
 }
 
 void lenv_add_builtins(lenv *e) {
+
+  /* Variable Functions */
+  lenv_add_builtin(e, "def", builtin_def);
+
   /* List Functions */
   lenv_add_builtin(e, "list", builtin_list);
   lenv_add_builtin(e, "head", builtin_head);
