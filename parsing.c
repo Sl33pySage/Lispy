26332601c3333830a8a4a0fea51fc3e1cc7ebd3f
Sl33pySage <jasonwolf82@hotmail.com>
2024-08-02 10:55:33 -0500

    On Variable Arguments section

--- parsing.c
+++ parsing.c
@@ -586,6 +586,49 @@ lval *builtin_var(lenv *e, lval *a, char *func) {
   return lval_sexpr();
 }
 
+lval *lval_call(lenv *e, lval *f, lval *a) {
+  /* If Builtin then simply call that */
+  if (f->builtin) {
+    return f->builtin(e, a);
+  }
+  /* Record Argument Counts */
+  int given = a->count;
+  int total = f->formals->count;
+
+  while (a->count) {
+    /*If we've ran out of formal arguments to bind */
+    if (f->formals->count == 0) {
+      lval_del(a);
+      return lval_err("Function passed too many arguments. "
+                      "Got %i, Expected %i.",
+                      given, total);
+    }
+    /* Pop the next argument from the list */
+    lval *val = lval_pop(a, 0);
+    /*Bind a copy into the function's environment */
+    lval_del(a);
+    /* If all formals have been bound evaluate */
+    if (f->formals->count == 0) {
+      /* Set environment parent to evaluation environment */
+      f->env->par = e;
+      /* Evaluate and return */
+      return builtin_eval(f->env, lval_add(lval_sexpr(), lval_copy(f->body)));
+    } else {
+      /* Otherwise return partially evaluated function */
+      return lval_copy(f);
+    }
+  }
+
+  for (int i = 0; i < a->count; i++) {
+    lenv_put(f->env, f->formals->cell[i], a->cell[i]);
+  }
+  lval_del(a);
+  /* Set the parent environment */
+  f->env->par = e;
+  /* Evaluate the body */
+  return builtin_eval(f->env, lval_add(lval_sexpr(), lval_copy(f->body)));
+}
+
 lval *builtin_lambda(lenv *e, lval *a) {
   /* Check Two arguments, each of which are Q-Expressions */
   LASSERT_NUM("\\", a, 2);
@@ -638,7 +681,7 @@ lval *lval_eval_sexpr(lenv *e, lval *v) {
   }
 
   /* If so call function to get result */
-  lval *result = f->builtin(e, v);
+  lval *result = lval_call(e, f, v);
   lval_del(f);
   return result;
 }
