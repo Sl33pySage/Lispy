3b6906034e769f0ab40644838c9f56fe9dc3c53c
Sl33pySage <jasonwolf82@hotmail.com>
2024-08-21 16:31:13 -0500

    on interesting functions

--- parsing.c
+++ parsing.c
@@ -501,8 +501,23 @@ lval *lval_call(lenv *e, lval *f, lval *a) {
   /* Argument list is now bound so can be cleaned up */
   lval_del(a);
 
-  /* If all formals have been bound evaluate */
-  if (f->formals->count == 0) {
+  /* If '&' remains in formal list bind to empty list */
+  if (f->formals->count > 0 && strcmp(f->formals->cell[0]->sym, "&") == 0) {
+    /* Check to ensure that & is not passed invalidity */
+    if (f->formals->count != 2) {
+      return lval_err("Function format invalid. "
+                      "Symbol '&' not followed by single symbol.");
+    }
+    /* Pop and delete '&' symbol */
+    lval_del(lval_pop(f->formals, 0));
+    /* Pop next symbol and create empty list */
+    lval *sym = lval_pop(f->formals, 0);
+    lval *val = lval_qexpr();
+
+    /* Bind to environment and delete */
+    lenv_put(f->env, sym, val);
+    lval_del(sym);
+    lval_del(val);
     /* Set invironment parent to evaluation environment */
     f->env->par = e;
     /* Evaluate and return */
